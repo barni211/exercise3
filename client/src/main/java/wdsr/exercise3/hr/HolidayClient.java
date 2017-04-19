081d86e8dbb28a5de9611db6f924c05c5d337f74
barni211 <barni211@gmail.com>
2017-04-19 22:08:32 +0200

    fight until you win - part 2

--- client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
+++ client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
@@ -1,8 +1,22 @@
 package wdsr.exercise3.hr;
 
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import wdsr.exercise3.hr.ws.EmployeeType;
+import wdsr.exercise3.hr.ws.HolidayRequest;
+import wdsr.exercise3.hr.ws.HolidayResponse;
+import wdsr.exercise3.hr.ws.HolidayType;
+import wdsr.exercise3.hr.ws.HumanResource;
 import wdsr.exercise3.hr.ws.HumanResourceService;
 
 // TODO Complete this class to book holidays by issuing a request to Human Resource web service.
@@ -31,7 +45,59 @@ public class HolidayClient {
 	 * @throws ProcessingException if request processing fails.
 	 */
 	public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate) throws ProcessingException {
-		// TODO
-		return 0;
+		HolidayRequest holidayRequest = new HolidayRequest();
+		
+		HolidayType holidayType = new HolidayType();
+		
+		try {
+			holidayType.setEndDate(getCalendarValue(endDate));
+			holidayType.setStartDate(getCalendarValue(startDate));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			
+			e.printStackTrace();
+			throw new ProcessingException();
+		} catch (DatatypeConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new ProcessingException();
+		}
+		holidayRequest.setHoliday(holidayType);
+		
+		EmployeeType employeeType = new EmployeeType();
+		employeeType.setFirstName(firstName);
+		employeeType.setLastName(lastName);
+		employeeType.setNumber(employeeId);
+		holidayRequest.setEmployee(employeeType);
+		
+		
+		
+		HumanResource humanResource = humanResourceService.getHumanResourcePort();
+		try {
+			HolidayResponse holidayResponse;
+			holidayResponse = humanResource.holiday(holidayRequest);
+			return holidayResponse.getRequestId();
+		} catch (Exception e) {
+			throw new ProcessingException();
+}
+		
+	}
+	
+	public XMLGregorianCalendar getCalendarValue(Date date) throws ParseException, DatatypeConfigurationException
+	{
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setFirstDayOfWeek(Calendar.MONDAY);
+		calendar.set(Calendar.MONTH, 6);
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		calendar.set(Calendar.YEAR, 2013);
+		
+		 XMLGregorianCalendar calendarToReturn = DatatypeFactory.newInstance()
+		            .newXMLGregorianCalendar(calendar);
+		    
+		return calendarToReturn;
 	}
 }

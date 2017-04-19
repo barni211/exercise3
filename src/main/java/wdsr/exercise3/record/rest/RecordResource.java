abac31ede47a65139d33c92ee694ab099dd30698
barni211 <barni211@gmail.com>
2017-04-19 21:23:09 +0200

    Fight until you win

--- src/main/java/wdsr/exercise3/record/rest/RecordResource.java
+++ src/main/java/wdsr/exercise3/record/rest/RecordResource.java
@@ -1,11 +1,30 @@
 package wdsr.exercise3.record.rest;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import wdsr.exercise3.record.Record;
 import wdsr.exercise3.record.RecordInventory;
 
 @Path("/records")
@@ -23,38 +42,35 @@ public class RecordResource {
 	 * API invocations that must be supported:
 	 */
 	
-	/**
-	 * * GET https://localhost:8091/records
-	 * ** Returns a list of all records (as application/xml)
-	 * ** Response status: HTTP 200
-	 */
 
-	/**
-	 * POST https://localhost:8091/records
-	 * ** Creates a new record, returns ID of the new record.
-	 * ** Consumes: record (as application/xml), ID must be null.
-	 * ** Response status if ok: HTTP 201, Location header points to the newly created resource.
-	 * ** Response status if submitted record has ID set: HTTP 400
-	 */
 	
-	/**
-	 * * GET https://localhost:8091/records/{id}
-	 * ** Returns an existing record (as application/xml)
-	 * ** Response status if ok: HTTP 200
-	 * ** Response status if {id} is not known: HTTP 404
-	 */
-	 /**
-	 * * PUT https://localhost:8091/records/{id}
-	 * ** Replaces an existing record in entirety.
-	 * ** Submitted record (as application/xml) must have null ID or ID must be identical to {id} in the path.
-	 * ** Response status if ok: HTTP 204
-	 * ** Response status if {id} is not known: HTTP 404
-	 */
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getRecords()
+	{
+		List<Record> results = new ArrayList<>();
+		results.addAll(recordInventory.getRecords());
+		GenericEntity<List<Record>> genericResult = new GenericEntity<List<Record>>(results) {};
+		return Response.ok(genericResult).build();
+	}
+
+	@POST	@Consumes(MediaType.APPLICATION_XML)	@Produces(MediaType.APPLICATION_XML)	public Response addRecord(Record record, @Context UriInfo uriInfo) {		if (record.getId() != null) {			return Response.status(Status.BAD_REQUEST).build();		}		recordInventory.addRecord(record);		UriBuilder uriB = uriInfo.getAbsolutePathBuilder();		uriB.path(Integer.toString(record.getId()));		return Response.created(uriB.build()).build();	}	
 	
-	/**
-	 * * DELETE https://localhost:8091/records/{id}
-	 * ** Deletes an existing record.
-	 * ** Response status if ok: HTTP 204
-	 * ** Response status if {id} is not known: HTTP 404
-	 */
+	
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getRecord(@PathParam(value = "id") int id) {
+		
+		Record record = recordInventory.getRecord(id);
+		if(record!=null)
+		{
+			return Response.ok(record).build();
+		}
+		return Response.status(Status.NOT_FOUND).build();
+	} 
+	
+	 	@PUT	@Path("/{id}")	@Produces(MediaType.APPLICATION_XML)	public Response putRecord(Record record, @PathParam(value = "id") int id) {				if (record.getId() != null &&  record.getId() != id) 		{			return Response.status(Status.BAD_REQUEST).build();		}		else if(recordInventory.updateRecord(id, record)) 		{			return Response.noContent().build();		}		return Response.status(Status.NOT_FOUND).build();	}
+			@DELETE	@Consumes(MediaType.APPLICATION_XML)	@Produces(MediaType.APPLICATION_XML)	@Path("/{id}")	public Response deleteRecord(@PathParam(value = "id") int id) {		if(recordInventory.deleteRecord(id)){			return Response.noContent().build();		}		return Response.status(Status.NOT_FOUND).build();	} 
 }
